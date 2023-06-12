bf11ba189bc3b0d2ef78d34485a0392afe8235a4
olesjack <olesjack@olesjack.com>
2023-06-11 20:49:52 -0500

    limit 20, images inside box

--- images.js
+++ images.js
@@ -15,16 +15,28 @@ $(document).ready(function () {
 
 	if (uu) query = query.where("uu", "==", uu);
 
-	query
-		.get()
-		.then((querySnapshot) =>
-			querySnapshot.forEach((doc) => addCard({ ...doc.data(), id: doc.id }))
+	query = query.limit(200);
+
+	query.get().then((querySnapshot) => {
+		querySnapshot.docs.forEach((doc, i) =>
+			addCard({ ...doc.data(), id: doc.id, index: i })
 		);
+
+		$(".ver-imgs").on("click", function () {
+		//	$(".imgs-box").html("");
+			const id = $(this).data("id");
+			console.log("id1", id);
+			showImages(id);
+		});
+	});
 });
 
-function showImages(image) {
-	$("#detail").html(
-		`<div>${image.images
+function showImages(id) {
+	const images = $("#imgs-" + id)
+		.data("imgs")
+		.split(",");
+	$("#imgs-" + id).html(
+		`<div>${images
 			.map(
 				(img, i) =>
 					`<img title="${
@@ -39,16 +51,20 @@ function addCard(image) {
 	$("#images").append(`
 <div class="card" style="width: 18rem;">
   <div class="card-body">
-    <h5 class="card-title">${image.title}</h5>
+    <h5 class="card-title">${image.index + 1}). ${image.title}</h5>
     <h6 class="card-subtitle mb-2 text-body-secondary"><a href="${image.url}">${
 		image.host
 	}</a></h6>
-    <p class="card-text"><a href="images?id=${image.id}">ver imagenes (${
-		image.imagesLength
-	})</a></p>
+    <p class="card-text">
+  			<button class="ver-imgs"  data-id="${image.id}">Ver imagenes (${image.imagesLength})</button>
+    </p>
+
     <p>${image.uu}</p><p title="${dayjs(image.date.toDate()).format(
 		"DD/MM/YYYY hh:mm:ss"
 	)}">${dayjs(image.date.toDate()).fromNow()}</p>
+    <span id="imgs-${image.id}" class="imgs-box" data-imgs="${image.images.join(
+		","
+	)}"></span>
   </div>
 </div>`);
 }
